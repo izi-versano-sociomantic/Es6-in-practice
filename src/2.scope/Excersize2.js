abdf04d170fd3ddce0610ae1bcc6e43a664c1ce6
izi.versano@sociomantic.com <izi.versano@sociomantic.com>
2017-03-21 17:21:04 +0100

    :zap:Function scope, block scope, constants and Excersizes

---
+++ src/2.scope/Excersize2.js
@@ -0,0 +1,28 @@
+var guessMe1 = 1;
+{
+	let guessMe2 = 3;
+	try
+    {
+		console.log( guessMe1, guessMe2 ); // (A)
+	}
+	catch ( _ )
+    {
+
+	}
+
+	console.log( guessMe1, guessMe2 ); // (B)
+
+}
+
+let guessMe2 = 2;
+console.log( guessMe1, guessMe2 ); // (C)
+
+var printer = () => {
+	var guessMe1 = 5;
+	console.log( guessMe1 ); // (D)
+	let guessMe2 = 6;
+	console.log( guessMe1, guessMe2 ); // (E)
+};
+
+printer();
+console.log( guessMe1, guessMe2 ); // (F)
